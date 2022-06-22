709de88d3a8a03e7a6685b76967cf73f307b183b
luochenhao <lch2023@foxmail.com>
2022-06-22 09:44:41 +0800

    新增员工功能、新增员工用户名重复异常处理

--- src/main/java/com/itcast/reggie/controller/EmployeeController.java
+++ src/main/java/com/itcast/reggie/controller/EmployeeController.java
@@ -13,6 +13,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.servlet.http.HttpServletRequest;
+import java.time.LocalDateTime;
 
 @Slf4j
 @RestController
@@ -68,5 +69,29 @@ public class EmployeeController {
     public R<String> logout(HttpServletRequest request) {
         request.getSession().removeAttribute("employee");//清理Session中保存的当前登录员工的id
         return R.success("退出成功");
+    }
+
+    /**
+     * 新增员工
+     * @param request
+     * @param employee
+     * @return
+     */
+    @PostMapping
+    public R<String> save(HttpServletRequest request, @RequestBody Employee employee) {//JSON形式的数据需要添加@RequestBody注解才能正常封装
+        log.info("新增员工，收集到的员工信息：{}", employee.toString());
+        //设置初始密码123465进行md5加密处理
+        employee.setPassword(DigestUtils.md5DigestAsHex("123456".getBytes()));
+
+        employee.setCreateTime(LocalDateTime.now());
+        employee.setUpdateTime(LocalDateTime.now());
+
+        //获取当前登录用户的id
+        Long empId = (Long) request.getSession().getAttribute("employee");
+        employee.setCreateUser(empId);
+        employee.setUpdateUser(empId);
+
+        employeeService.save(employee);//save方法继承了MybatisPlus父接口IService直接调用即可
+        return R.success("新增员工成功");
     }
 }
