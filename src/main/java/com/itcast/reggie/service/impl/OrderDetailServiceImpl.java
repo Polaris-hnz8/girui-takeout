63a30c26defdb932ef1e4969d28ab91065e5b47b
luochenhao <lch2023@foxmail.com>
2022-07-07 21:44:17 +0800

    feat(order-submit):Realize the function of submit an order submit操作会涉及到数据库中的3张table： 1.订单表orders数据插入、 2.订单明细表order_details数据插入、 3.购物车shopping_cart数据删除

---
+++ src/main/java/com/itcast/reggie/service/impl/OrderDetailServiceImpl.java
@@ -0,0 +1,14 @@
+package com.itcast.reggie.service.impl;
+
+import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com.itcast.reggie.entity.OrderDetail;
+import com.itcast.reggie.mapper.OrderDetailMapper;
+import com.itcast.reggie.service.OrderDetailService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+@Slf4j
+@Service
+public class OrderDetailServiceImpl extends ServiceImpl<OrderDetailMapper, OrderDetail> implements OrderDetailService {
+
+}
